2910e6d3e7478a6dc770c4c0ed14bda74293ee5e
lilith1995 <lilith.mnatsakanyan@gmail.com>
2021-09-14 01:25:42 +0400

    Changed the flex

---
+++ src/components/Mainsection/mainsection.jsx
@@ -0,0 +1,15 @@
+import "./mainsection.scss";
+
+export const MainSection = () => {
+  return (
+    <div className="mainsection">
+      <div className="maintext">
+        <h2 className="firsttext">Get Credit Card Debt Help</h2>
+        <h2 className="secondtext">From America's top debt relief company</h2>
+      </div>
+      <div className="mainpic">
+        <img className="amountpic" src="photo.png" alt="amount" />
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Textsection/textsection.jsx
@@ -0,0 +1,14 @@
+import "./textsection.scss";
+
+export const TextSection = () => {
+  return (
+    <div className="textsection">
+      <h1>Credid card debt relief</h1>
+      <p>
+        "It would have taken me 15 years to get out of debt. With Freedom, it's
+        only going to take me 3."
+      </p>
+      <cite>--Alejandra, current FDR client</cite>
+    </div>
+  );
+};
