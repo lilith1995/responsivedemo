c1ca0d36ea36fb9ba9b1510b788647471eed1f88
lilith1995 <lilith.mnatsakanyan@gmail.com>
2021-09-16 18:27:13 +0400

    added new styles

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,24 +1,25 @@
 import "./footer.scss";
 
+import person from "../../assets/person.svg";
+import cash from "../../assets/cash.svg";
+import check from "../../assets/cash.svg";
+
 export const Footer = () => {
   return (
     <div className="footer">
       <div className="components">
         <div className="column">
-          <img
-            src="https://www.freedomdebtrelief.com/next-assets/person.svg"
-            alt="person"
-          />
+          <img src={person} alt="Person" />
           <p className="text">Over 650.000 clients enrolled</p>
         </div>
         <div className="column">
-          <img src="/cash.svg" alt="cash" />
+          <img src={cash} alt="cash" />
           <p className="text">
             Free, no-risk credit card dept relief consultation
           </p>
         </div>
         <div className="column">
-          <img src="/check.svg" alt="check" />
+          <img src={check} alt="check" />
           <p className="text">Over $10 billion debt resolved since 2002</p>
         </div>
       </div>
